fb64cccb73f95f0792c2cec1e7073a6eb34c8760
aaksi <yygg91@mail.ru>
2020-12-29 14:02:56 +0300

    fix js

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,11 @@
-$('.documents__slider').slick({
+
+$(function(){
+  $('.documents__slider').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
     prevArrow:'<button type="button" class="slick-prev slider-btn"></button>',
     nextArrow:'<button type="button" class="slick-next slider-btn"></button>'
   });
+});
+

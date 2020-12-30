d6211a38519adc761138e0b47caaffabf663fb4a
aaksi <yygg91@mail.ru>
2020-12-30 17:01:54 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,41 @@ $(function(){
     slidesToShow: 3,
     slidesToScroll: 3,
     prevArrow:'<button type="button" class="slick-prev slider-btn"></button>',
-    nextArrow:'<button type="button" class="slick-next slider-btn"></button>'
+    nextArrow:'<button type="button" class="slick-next slider-btn"></button>',
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 630,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      }
+      
+    ]
+  });
+
+
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__btn').toggleClass('menu__btn--active');
+    $('.menu__list').toggleClass('menu__list--active')
   });
 });
 
